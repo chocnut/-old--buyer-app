95819d1cc4fda2bb9b8bde39e0c0082fe2a7e951
Crash <crash64bit@mail.ru>
2020-03-13 19:17:41 +0200

    Made calculations to the function

--- src/screens/Auth/LogInScreen.js
+++ src/screens/Auth/LogInScreen.js
@@ -16,6 +16,15 @@ import colors from "../../constants/Colors";
 import Btn from "../../components/Btn";
 import EewooInput from "../../components/EewooInput";
 
+
+const titleTop = () => {
+  return Layout.window.height > 667 ? Layout.window.height / 100 * 8 : Layout.window.height / 100 * 6;
+}
+
+const titleBottom = () => {
+  return Layout.window.height > 667 ? Layout.window.height / 100 * 8 : Layout.window.height / 100 * 6;
+}
+
 // @observer
 export default class LogInScreen extends React.Component {
   static navigationOptions = {
@@ -162,8 +171,8 @@ const styles = StyleSheet.create({
     fontFamily: "Quicksand-Medium",
     color: colors.graphite,
     textAlign: "center",
-    marginTop: Layout.window.height > 667 ? Layout.window.height / 100 * 8 : Layout.window.height / 100 * 6,
-    marginBottom: Layout.window.height > 667 ? Layout.window.height / 100 * 8 : Layout.window.height / 100 * 5,
+    marginTop: titleTop(),
+    marginBottom: titleBottom(),
   },
   textLink: {
     fontSize: 13,
