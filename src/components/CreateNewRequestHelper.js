ced37bd7588d26a289aaaa36d720b6109f1d438b
Crash <crash64bit@mail.ru>
2020-03-30 10:53:46 +0300

    Update CreateNewRequestHelper

--- src/components/CreateNewRequestHelper.js
+++ src/components/CreateNewRequestHelper.js
@@ -14,19 +14,19 @@ function CreateNewRequestHelper() {
       <View style={{flexGrow: 1}}></View>
       <Image
         style={styles.icon}
-        source={require('../../assets/images/folder.png')} />
+        source={require("../../assets/images/folder.png")} />
       <Text style={styles.title}>
         Create new request
       </Text>
       <Text style={styles.text}>
-        Please press the plus button {'\n'}
+        Please press the plus button {"\n"}
         to create your first request
       </Text>
       <SafeAreaView
         style={styles.arrowHolder}>
         <View
           style={styles.arrowInner}>
-          <Image source={require('../../assets/images/long-dashed-arrow.png')}
+          <Image source={require("../../assets/images/long-dashed-arrow.png")}
             style={styles.arrow}
           />
         </View>
@@ -40,8 +40,8 @@ export default CreateNewRequestHelper;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   icon: {
     width: 117,
@@ -55,33 +55,33 @@ const styles = StyleSheet.create({
     marginBottom: 12,
     fontSize: 24,
     lineHeight: 32,
-    textAlign: 'center',
+    textAlign: "center",
   },
   text: {
     color: colors.secondary,
     fontFamily: "Quicksand-Regular",
     fontSize: 14,
     lineHeight: 20,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 18,
   },
   arrowHolder: {
     flex: 1,
-    display: 'flex',
-    width: '51%',
-    alignSelf: 'flex-end',
+    display: "flex",
+    width: "51%",
+    alignSelf: "flex-end",
   },
   arrowInner: {
     flexGrow: 1,
-    resizeMode: 'stretch',
+    resizeMode: "stretch",
     paddingRight: 74, // buttonSize 54 + button margin 16 + arrow margin 4
     paddingBottom: 60, // buttonSize 54 + button margin 16 - arrow margin 4
   },
   arrow: {
     flexGrow: 1,
-    resizeMode: 'stretch',
-    width: 'auto',
-    height: 'auto',
+    resizeMode: "stretch",
+    width: "auto",
+    height: "auto",
   },
 })
 
